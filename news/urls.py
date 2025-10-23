067d8592869fadc30a57dbc672f3f55271c739f4
SebastianGranadosJ <juansebastiangranadosjaimes26@gmail.com>
2025-10-23 17:54:55 -0500

    .

--- news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NewsView, SourcesView
-
-urlpatterns = [
-    path("news/sources/",SourcesView.as_view(), name="get-sources"), 
-    path("news/",NewsView.as_view() , name="get-news"),
-]
--- stocks/serializers/metric_dto_serializer.py
+++
@@ -1,9 +0,0 @@
-# market/serializers.py
-from rest_framework import serializers
-
-class MetricDTOSerializer(serializers.Serializer):
-    ticker = serializers.CharField()
-    name = serializers.CharField()
-    price = serializers.FloatField(allow_null=True)
-    daily_change = serializers.FloatField(allow_null=True)
-    extra_metrics = serializers.DictField()
